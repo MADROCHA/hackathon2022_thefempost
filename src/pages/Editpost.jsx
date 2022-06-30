32232d662597b0478d78438afb77385d172669f7
DamarisTeoc <teoc.918@gmail.com>
2022-06-30 17:06:46 +0200

    editpost.jsx wip

--- src/pages/Editpost.jsx
+++ src/pages/Editpost.jsx
@@ -3,9 +3,10 @@ import Main from "../components/Main";
 import React,{useState} from 'react';
 
 
-const editPost = (props) => {
+const editPost = () => {
 
-    const [title, setTitle] = useState(props.title);
+    const [title, setTitle] = useState('');
+    const [category, setCategory] = useState('');
     return (
         <Main>
             <Link to="/">Go home</Link>
@@ -17,11 +18,11 @@ const editPost = (props) => {
 
                         <div class="flex flex-col space-y-2">
                             <label for="username"></label>
-                            <input class="border-violet-light border" placeholder="title" type="text" id="title" name="title" value="edit title"/>
+                            <input class="border-violet-light border" placeholder="title" type="text" id="title" name="title"/>
                             <label for="content"></label>
                             
                             <label for="title"></label>
-                            <input class="border-violet-light border" placeholder="category" type="text" id="category" name="category" value="deporte"/>
+                            <input class="border-violet-light border" placeholder="category" type="text" id="category" name="category"/>
                             
                             <button class="border-violet-light border hover:bg-violet-dark hover:text-white open-sans text-violet-dark text-xl justify-center flex place-items-center h-12 w-2/12 ">
                                     EDIT POST
