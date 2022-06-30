8976374e99b4f394705661a5ea618f2245ebae60
MADROCHA <madlarocha@gmail.com>
2022-06-30 12:02:05 +0200

    colors_tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     "*.html"
   ],
   theme: {
+    colors: {
+      white: "#F5F5F5",
+      highlight: "#0AC2FF",
+      violet: {
+        light: "#9A4C95",
+        dark: "#4D2D52" ,
+      },
+      black: "#1D1A31",
+
+    },
     extend: {},
   },
   plugins: [],
