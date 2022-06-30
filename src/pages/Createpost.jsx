9804759390b641f96008c9e280ad8fb7de52415e
MADROCHA <madlarocha@gmail.com>
2022-06-30 16:46:46 +0200

    jsx._Createpost/Edit

--- src/pages/Createpost.jsx
+++ src/pages/Createpost.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import Main from "../components/Main";
 
-const Login = () => {
+const Createpost = () => {
     return (
         <Main>
             <Link to="/">Go home</Link>
@@ -13,14 +13,14 @@ const Login = () => {
 
                         <div class="flex flex-col space-y-2">
                             <label for="username"></label>
-                            <input class="border-violet-light border" placeholder="User name" type="text" id="title" name="title"/>
+                            <input class="border-violet-light border" placeholder="title" type="text" id="title" name="title"/>
                             <label for="content"></label>
                             
                             <label for="title"></label>
-                            <input class="border-violet-light border" placeholder="password" type="text" id="category" name="category"/>
+                            <input class="border-violet-light border" placeholder="category" type="text" id="category" name="category"/>
                             
                             <button class="border-violet-light border hover:bg-violet-dark hover:text-white open-sans text-violet-dark text-xl justify-center flex place-items-center h-12 w-2/12 ">
-                                    LOGIN
+                                    CREATE POST
                             </button>
                         </div>
                     </form>
@@ -32,4 +32,4 @@ const Login = () => {
         )    
 }
 
-export default Login;
+export default Createpost;
--- src/routes/RoutesConfig.jsx
+++ src/routes/RoutesConfig.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Post from '../pages/Post';
 import Login from '../pages/Login';
+import Createpost from '../pages/Createpost';
 
 const RoutesConfig = () => {
   return (
@@ -10,6 +11,7 @@ const RoutesConfig = () => {
         <Route path="/" element={ <Home/> } />
         <Route path="/post/:id" element={ <Post/> } />
         <Route path="/login" element={ <Login/> } />
+        <Route path="/Createpost" element={ <Createpost/> } />
       </Routes>
     </Router>
   )
